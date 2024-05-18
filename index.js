4aad2ac38eafe96866c6db8e723f700a2b251699
itsmanik <manik.hilalpure@gmail.com>
2024-05-19 02:22:22 +0530

    added nav header/nav and made it responsive

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+const toggle = document.getElementById("nav-toggle"),
+    nav = document.getElementById("nav-menu");
+    toggle.addEventListener("click", () => {
+    nav.classList.toggle("show-menu");
+    toggle.classList.toggle("show-icon");
+});
